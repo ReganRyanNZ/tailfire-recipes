2fcb6fb77442414686edac1536a8791114a3b233
Regan Ryan <regan.ryan.nz@gmail.com>
2024-01-15 14:54:04 -0300

    bugfix config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-// // vite.config.js
-// import { spaFallback } from './vite-plugin-spa-fallback'
-// import { defineConfig } from 'vite'
+// vite.config.js
+import { spaFallback } from './vite-plugin-spa-fallback'
+import { defineConfig } from 'vite'
 
-// export default defineConfig({
-//   plugins: [spaFallback()],
-// })
+export default defineConfig({
+  // plugins: [spaFallback()],
+  plugins: [],
+})
