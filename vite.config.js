bf3bc83decae4649f4a5377be54357215d2e8de0
Regan Ryan <regan.ryan.nz@gmail.com>
2024-01-23 08:25:37 -0300

    updated service worker config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,46 @@
+import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default {
+export default defineConfig({
   plugins: [
     VitePWA({ registerType: 'autoUpdate',
-    devOptions: {
-      enabled: true
-    } })
+              devOptions: { enabled: true },
+              workbox: { globPatterns: ["**/*"] }, // cache all the imports
+              includeAssets: [ "**/*" ], // cache all the static assets in the public folder
+              manifest: {
+                "name": "Tailfire Recipes",
+                "short_name": "Recipes",
+                "start_url": "/",
+                "display": "standalone",
+                "theme_color": "#FFFFFF",
+                "background_color": "#FFFFFF",
+                "lang": "en",
+                "scope": "/",
+                "icons": [
+                  {
+                      "src": "/android-chrome-192x192.png",
+                      "sizes": "192x192",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/android-chrome-512x512.png",
+                      "sizes": "512x512",
+                      "type": "image/png"
+                  },
+                  {
+                    "src": "/maskable_icon_x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png",
+                    "purpose": "maskable"
+                  },
+                  {
+                    "src": "/maskable_icon_512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png",
+                    "purpose": "maskable"
+                  }
+                ]
+              }
+    })
   ]
-}
+})
