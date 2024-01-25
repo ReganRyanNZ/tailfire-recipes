14149e342969955606514a581839dfd8e00b30d9
Regan Ryan <regan.ryan.nz@gmail.com>
2024-01-25 09:38:29 -0300

    added pasta cutting technique to alfredo

--- recipes/fettuccineAlfredo.js
+++ recipes/fettuccineAlfredo.js
@@ -8,6 +8,7 @@ export const fettuccineAlfredo = {
     {ingredients: [`70g *butter*`, `45g grated *parmesan cheese*`, `1/2 cup *pasta water*`],
      actions: [`Melt butter in a pan, add noodles and parmesan, stir together.`, `Slowly add the pasta water while stirring to get a creamy sauce.`]},
   ],
-  tips: [`This is the plain traditional recipe, but you can build on it with bacon, mushrooms, or sundried tomatoes.`
+  tips: [`This is the plain traditional recipe, but you can build on it with bacon, mushrooms, or sundried tomatoes.`,
+         `For great pasta, it's worthwhile to get a pasta machine. If you don't have one, you can roll the dough very flat, then fold the dough sheet in half several times (in the same direction) and slice with a knife to cut the noodles.`
   ]
 }
