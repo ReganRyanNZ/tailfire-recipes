9e48ba1db3eea9efa1feb9674537c4f050b9ef76
Regan Ryan <regan.ryan.nz@gmail.com>
2024-01-25 09:34:55 -0300

    better measurements for alfredo

--- recipes/fettuccineAlfredo.js
+++ recipes/fettuccineAlfredo.js
@@ -1,13 +1,13 @@
 export const fettuccineAlfredo = {
   title: `Fettuccine Alfredo`,
   steps: [
-    {ingredients: [`2 cups *flour*`, `3 *eggs*`],
+    {ingredients: [`300g *flour*`, `3 *eggs*`],
      actions: [`Mix together and knead until very smooth.`, `Roll out thin and cut into fettuccine strips.`]},
     {ingredients: [`*Salt*`],
      actions: [`Cook noodles for 60 seconds in salted water.`, `Remove the water but keep 1/2 cup aside for the next step.`]},
-    {ingredients: [`5 tbsp *butter*`, `1/2 cup grated *parmesan cheese*`, `1/2 cup *pasta water*`],
-     actions: [`Melt butter in a pan, add noodles and parmesan, stir together.`, `Slowly add the pasta water to get a creamy sauce.`]},
+    {ingredients: [`70g *butter*`, `45g grated *parmesan cheese*`, `1/2 cup *pasta water*`],
+     actions: [`Melt butter in a pan, add noodles and parmesan, stir together.`, `Slowly add the pasta water while stirring to get a creamy sauce.`]},
   ],
-  tips: [`A more accurate measurement for pasta is 100g flour to 1 egg. `
+  tips: [`This is the plain traditional recipe, but you can build on it with bacon, mushrooms, or sundried tomatoes.`
   ]
 }
