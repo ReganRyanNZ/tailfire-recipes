369b3b11c246eb2155b8304df90c241850c1891e
Regan Ryan <regan.ryan.nz@gmail.com>
2024-01-30 21:21:17 -0300

    added choc self sauce pudding

---
+++ pages/recipes/selfSaucingPudding.js
@@ -0,0 +1,29 @@
+export const selfSaucingPudding = `
+Chocolate Self-Saucing Pudding
+Serves 6-8
+30 minutes
+
+- 1 cup *flour*
+- 2 tsp *baking powder*
+- 1/4 tsp *salt*
+- 1/2 cup *sugar*
+- 1 tbsp *cocoa powder*
+Mix together in a large bowl.
+
+- 2 tbsp *melted butter*
+- 1/2 cup *milk*
+- 1 tsp *vanilla essence*
+Add to the bowl and mix together.
+Pour the mixture into a greased baking dish
+
+- 1/2 cup *brown sugar*
+- 2 tbsp *cocoa powder*
+- 2 1/4 cup *hot water*
+Sprinkle sugar then cocoa powder evenly on top (do not mix it in).
+Carefully pour the water over the top and put it in the oven at 180ºC for 20 mins.
+
+tips
+The ingredients of the last step should not be stirred in, they will become a rich chocolate sauce for the pudding.
+Don't worry if it seems very wet—that's by design. The key to knowing if it's done is looking at the texture in the center, rather than the outside.
+Serve with a high-quality vanilla ice cream.
+`
