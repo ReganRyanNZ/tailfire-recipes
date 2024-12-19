274e9931ef6ce280288224c27263e8f70cb2b955
Regan Ryan <regan.ryan.nz@gmail.com>
2024-12-18 21:29:43 -0700

    tips update for chog mug cake

--- pages/recipes/chocMugCake.js
+++ pages/recipes/chocMugCake.js
@@ -27,6 +27,6 @@ Don't worry if it seems very wet—that's by design. The key to knowing if it's 
 The ingredients of the last step should not be stirred in, they will become the chocolate sauce for the cake.
 Any oil can be used, but avoid strong-tasting oils like olive oil.
 Microwaves vary in strength, you will need to find the optimal time for your microwave.
-This recipe uses so many small measurements (e.g. 1.2g of baking powder), so we are keeping things in tsp/tbsp for this one :)
+This recipe uses many small measurements (e.g. 1.2g of baking powder), so for those who don't have high-precision scales, we are keeping things in tsp/tbsp for this one :)
 Serve with a high-quality vanilla ice cream.
 `
